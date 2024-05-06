db625c463db8a73ce6a79cfc15190565581f25eb
erick seis <erickseis@gmail.com>
2024-05-05 20:03:27 -0400

    carga nueva

--- src/db.js
+++ src/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
+const db_crud = 'mongodb+srv://erickseis:G6P2mDnd82d1gYF7@cluster0.h6gdz9i.mongodb.net/db_crud?retryWrites=true&w=majority&appName=Cluster0'
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost/merndb')
+        await mongoose.connect(db_crud)
         console.log(">>> DB is connected")
 
     } catch (error) {
